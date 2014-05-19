1d15b9aef4941eca860c8ee994f1ec2b9eb87154
confi <confi@outlook.com>
2014-05-19 12:33:05 +0800

    First commit

---
+++ OrderList/My Project/AssemblyInfo.vb
@@ -0,0 +1,52 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports System.Security
+Imports Microsoft.Office.Tools.Excel
+
+' 有关程序集的常规信息通过下列属性集
+' 控制。更改这些属性值可修改
+' 与程序集关联的信息。
+
+' 查看程序集属性的值
+
+<Assembly: AssemblyTitle("OrderList")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("OrderList")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+' 将 ComVisible 设置为 false 使此程序集中的类型
+' 对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型，
+' 则将该类型上的 ComVisible 属性设置为 true。
+<Assembly: ComVisible(False)>
+
+'如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+<Assembly: Guid("a08e590a-7a0c-48a1-b126-222ce2bb5516")> 
+
+' 程序集的版本信息由下面四个值组成:
+'
+'      主版本
+'      次版本 
+'      内部版本号
+'      修订号
+'
+' 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
+' 方法是按如下所示使用“*”:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
+
+' ExcelLocale1033 属性用于控制传递给 Excel 对象模型的区域设置。
+' 将 ExcelLocale1033 设置为 true 会使 Excel 对象模型在
+' 所有区域设置下采取同一行为，该行为与 Visual Basic for  Applications
+' 的行为相符。将 ExcelLocale1033 设置为 false 会使 Excel 对象模型
+' 的行为随用户的区域设置不同而不同，该行为
+' 与 Visual Studio Tools for Office 版本 2003 的行为相符。这会导致
+' 区分区域设置的信息(如公式名和日期格式)中出现意外结果。
+
+<Assembly: ExcelLocale1033(True)>
+
+<Assembly: SecurityTransparent()>
---
+++ OrderList/ThisAddIn.vb
@@ -0,0 +1,75 @@
+﻿Imports Microsoft.Office.Interop.Excel
+
+Public Class ThisAddIn
+
+    Private Sub ThisAddIn_Startup(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Startup
+        makeOrderList()
+    End Sub
+
+    Private Sub ThisAddIn_Shutdown(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Shutdown
+
+    End Sub
+
+
+    Private Sub makeOrderList()
+        Dim listWorkbook As Workbook
+        Dim listWorksheet As Worksheet
+        Dim listRange As Range
+        listWorkbook = Me.Application.ActiveWorkbook
+
+        Dim lastRow As Integer
+
+
+        For Each listWorksheet In listWorkbook.Worksheets
+            lastRow = listWorksheet.Range("print_area").Rows.Count
+            Select Case listWorksheet.Name
+                Case "设备清单"
+
+                    listRange = listWorksheet.Range("A9:J" & CStr(lastRow))
+                    listWorksheet.Range("A8").Value = "数量"
+                    listWorksheet.Range("F8").Value = "编号"
+                    combine(listRange, "C", "F")
+
+                Case "仪表清单"
+                    listRange = listWorksheet.Range("A9:L" & CStr(lastRow))
+                    listWorksheet.Range("A8").Value = "数量"
+                    listWorksheet.Range("L8").Value = "编号"
+                    combine(listRange, "DE", "L")
+                    listWorksheet.Columns(2).delete()
+                Case "阀门清单"
+                    listRange = listWorksheet.Range("A9:H" & CStr(lastRow))
+                    listWorksheet.Range("A8").Value = "数量"
+                    combine(listRange, "BC", "H")
+            End Select
+            
+        Next
+    End Sub
+
+    Sub combine(ByVal combineRange As Range, ByVal criteriaCol As String, ByVal codeCol As String)
+        Dim data(,) As Object = combineRange.Value
+        Dim criteria() As Integer = {Asc(Left(criteriaCol, 1)) - 64, Asc(Right(criteriaCol, 1)) - 64}
+        Dim code As Integer = Asc(codeCol) - 64
+        Dim i As Integer
+        Dim j As Integer
+        Dim k As Integer
+        Dim counter As Integer = 1
+
+
+        For i = 1 To UBound(data, 1)
+            data(i, code) = data(i, 1)
+            For j = UBound(data, 1) To i + 1 Step -1
+                If data(i, criteria(0)) = data(j, criteria(0)) And data(i, criteria(1)) = data(j, criteria(1)) Then
+                    counter = counter + 1
+                    data(i, code) = data(i, code) & data(j, 1)
+                    For k = 1 To UBound(data, 2)
+                        data(j, k) = ""
+                    Next
+                End If
+            Next
+            data(i, 1) = counter
+            counter = 1
+        Next
+
+
+    End Sub
+End Class
